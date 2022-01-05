ee4f1755ad92d98d45bc4c30450cb0f02293eb15
Ömer Iliski <omeriliski06@gmail.com>
2022-01-05 20:31:32 +0100

    fixed table

--- firebase.js
+++ firebase.js
@@ -1,5 +1,3 @@
-// $(".table-body").load("tr.html");
-// document.querySelector(".navbar-container").load("navbar.html");
 $(".navbar-container").load("navbar.html");
 $(".table-container").load("table.html");
 const inputGerman = document.getElementById("german");
@@ -9,7 +7,7 @@ const inputSentence = document.getElementById("sentence");
 
 let wordsArr;
 let usersArr;
-let cardNo=1;
+let cardNo = 1;
 
 //initialize firebase
 const firebaseConfig = {
@@ -67,225 +65,201 @@ const cloudDB = firebase.firestore();
 // }
 // getData();
 
-
+const getWords = new Promise((resolve, reject) => {
+  wordsArr = [];
+  cloudDB.collection("words").get()
+    .then(res => {
+      res.docs.forEach(doc => {
+        const data = doc.data();
+        data["documentId"] = doc.ref.id;
+        wordsArr.push(data);
+        wordsArr.sort(GetSortOrder("german"));
+      })
+      resolve(wordsArr)
+    })
+    .catch(err => reject(err))
+})
 
 const saveWord = () => {
-  if(inputGerman.value.length !=0 && inputTurkish.value.length !=0 && inputSentence.value.length !=0){
-    const data={
-      id:Date.now(),
+  if (inputGerman.value.length != 0 && inputTurkish.value.length != 0 && inputSentence.value.length != 0) {
+    const data = {
+      id: Date.now(),
       german: inputGerman.value,
       turkish: inputTurkish.value,
       sentence: inputSentence.value,
-      rating:0,
+      rating: 0,
     }
     cloudDB.collection("words").add(data)
-    .then((result) => {
-      inputGerman.value="";
-      inputTurkish.value="";
-      inputSentence.value="";
-      addRow(data);
-      document.location.reload(true)
-    }).catch((err) => {
-      console.log('save err :>> ', err);
-    });
-  }
-  else alert("Bitte füllen Sie alles");
+      .then((docRef) => {
+        inputGerman.value = "";
+        inputTurkish.value = "";
+        inputSentence.value = "";
+        data.documentId=docRef.id;
+        wordsArr.push(data);
+        console.log('saveWord wordsArr :>> ', wordsArr);
+        setTable()
+      }).catch((err) => {
+        console.log('save err :>> ', err);
+      });
+  } else alert("Bitte füllen Sie alles");
 }
 
-const deleteWord=(data)=>{
-  cloudDB.collection("words").doc(data.documentId).delete().
-  then(()=>document.location.reload(true))
-  
-}
-
-const addRow=(data)=>{
-  const tableRow = document.getElementById("table-row");
-  const clone = tableRow.cloneNode(true);
-  
-  clone.setAttribute("id",`table-row-${data.id}`);
-  
-  clone.getElementsByClassName("german-word")[0].innerHTML = data.german;
-  clone.getElementsByClassName("turkish-word")[0].innerHTML = data.turkish;
-  clone.getElementsByClassName("sentence-word")[0].innerHTML = data.sentence;
-  clone.getElementsByClassName("delete-word-button")[0].innerHTML="DELETE"
-  clone.getElementsByClassName("rating-word")[0].innerHTML = "";
-  
-  //delete Item
-  clone.getElementsByClassName("delete-word-button")[0].addEventListener("click",()=>{
-    deleteWord(data)
-  })
-  
-  const ratingWord = clone.getElementsByClassName("rating-word")[0];
-  for (let i = 0; i < data.rating; i++) {
-    const rating = document.createElement("i");
-    rating.classList.add("text-yellow-500","fas","fa-star");
-    ratingWord.appendChild(rating);
-  }
-  document.getElementsByClassName("table-body")[0].appendChild(clone);
-  
-}
-
-function GetSortOrder(prop) {    
-  return function(a, b) {    
-      if (a[prop] > b[prop]) {    
-          return 1;    
-      } else if (a[prop] < b[prop]) {    
-          return -1;    
-      }    
-      return 0;    
-  }    
-} 
-
-
-const getWords=new Promise((resolve,reject)=>{
-  wordsArr=[];
-  cloudDB.collection("words").get()
-  .then(res=>{
-    res.docs.forEach(doc => {
-      const data=doc.data();
-      data["documentId"]=doc.ref.id;
-      wordsArr.push(data);
-      wordsArr.sort(GetSortOrder("german"));
-    })
-    resolve(wordsArr)
-  })
-  .catch(err=>reject(err))  
-})
-
-const setTable = ()=>{
+const deleteWord = (data) => {
+  cloudDB.collection("words").doc(data.documentId).delete()
+  .then(()=>{
     getWords
-    .then(res=>{
-      res.forEach(e=>{
-        addRow(e)
+    console.log('deleted :>> ');
+  })  //update the wordsArr
+  .catch(err=>console.log('deleteWord err :>> ', err))
+}
+
+const addRow = (data) => {
+
+  let starIcons="";
+  for (let i = 0; i < data.rating; i++) {
+    starIcons+=`<i class="text-yellow-500 fas fa-star"></i>`
+  }
+  const tableRow=
+  `<tr id="table-row-${data.id}" class="table-row">
+    <td class="px-6 py-4 whitespace-nowrap w-1/6">
+      <div class="flex items-center">
+        <div class="ml-4">
+          <div id="german-word" class="german-word text-sm font-medium text-gray-900">
+            ${data.german}
+          </div>
+        </div>
+      </div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap w-1/6">
+      <div id="turkish-word" class="turkish-word text-sm text-gray-900">${data.turkish}</div>
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap w-1/3">
+      <span id="sentence-word" class="sentence-word text-sm text-gray-900">${data.sentence}</span>
+    </td>
+    <td id="rating-word" class="rating-word px-6 py-4 whitespace-nowrap text-sm text-gray-500">
+      ${starIcons}
+    </td>
+    <td class="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+      <button id="delete-word-${data.id}" class="text-indigo-600 hover:text-indigo-900">DELETE</button>
+    </td>
+  </tr>`
+  document.querySelector("#table-body").innerHTML += tableRow;
+}
+
+const setTable = () => {
+  document.getElementsByClassName("table-body")[0].innerHTML = "";
+  wordsArr.forEach(word => {
+    addRow(word)
+  })
+  //addeventlistener to delete buttons
+  wordsArr.forEach(word=>{
+      document.querySelector(`#delete-word-${word.id}`).addEventListener("click",()=>{
+        wordsArr = wordsArr.filter(e=> e != word)
+        console.log('delete wordsArr:>> ',wordsArr);
+        deleteWord(word);
+        setTable();
       })
-      const row = document.getElementById("table-row").classList.add("hidden");
-    })
+  })
 }
-const addStarToCard=()=>{
-  let ratingCard=document.getElementById("card-container-1").getElementsByClassName("rating-card")[0]
+
+function GetSortOrder(prop) {
+  return function (a, b) {
+    if (a[prop] > b[prop]) {
+      return 1;
+    } else if (a[prop] < b[prop]) {
+      return -1;
+    }
+    return 0;
+  }
+}
+
+const addStarToCard = () => {
+  let ratingCard = document.getElementById("card-container-1").getElementsByClassName("rating-card")[0]
   let rating = document.createElement("i");
-  rating.classList.add("text-yellow-500","fas","fa-star");
+  rating.classList.add("text-yellow-500", "fas", "fa-star");
   ratingCard.appendChild(rating);
-  // console.log('ratingCard :>> ', ratingCard);  
   console.log('rating :>> ', rating);
-  rating="";
+  rating = "";
 }
-const deleteStartFromCard=()=>{
-  const ratingCard=document.getElementById("card-container-1").getElementsByClassName("rating-card")[0]
+const deleteStarFromCard = () => {
+  const ratingCard = document.getElementById("card-container-1").getElementsByClassName("rating-card")[0]
   ratingCard.lastChild.remove();
 }
 
-
-const compare=(inputValue)=>{
+const compare = (inputValue) => {
   console.log('compare  wordsArr:>> ', wordsArr);
-  if(wordsArr[cardNo].turkish==inputValue){
+  if (wordsArr[cardNo].turkish == inputValue) {
     console.log('bravo :>> ')
-    if(wordsArr[cardNo].rating<5){
+    if (wordsArr[cardNo].rating < 5) {
       wordsArr[cardNo].rating++
       addStarToCard()
     }
-  }
-  else {
+  } else {
     console.log('leider :>> ');
-    if(wordsArr[cardNo].rating>0){
+    if (wordsArr[cardNo].rating > 0) {
       wordsArr[cardNo].rating--
-      deleteStartFromCard();
+      deleteStarFromCard();
     }
   }
   cloudDB.collection("words").doc(wordsArr[cardNo].documentId).update(wordsArr[cardNo]);
-  // const checkButton = document.getElementById("card-container-1").getElementsByClassName("check-button")[0].classList.add("hidden")
 }
 
 
+const addCard = (cardDiv, no) => {
+  const germanCardWord = document.getElementById(cardDiv).getElementsByClassName("german-card-word")[0]
+  germanCardWord.innerHTML = wordsArr[no].german
 
-const addToCard=()=>{
-  getWords
-  .then(res=>{
-    let germanCardWord=document.getElementById("card-container-1").getElementsByClassName("german-card-word")[0]
-    germanCardWord.innerHTML=res[cardNo].german
+  const turkishCardWord = document.getElementById(cardDiv).getElementsByClassName("turkish-card-word")[0]
+  turkishCardWord.innerHTML = wordsArr[no].turkish
 
-    let turkishCardWord=document.getElementById("card-container-1").getElementsByClassName("turkish-card-word")[0]
-    turkishCardWord.innerHTML=res[cardNo].turkish
-
-    let sentenceCardWord=document.getElementById("card-container-1").getElementsByClassName("sentence-card-word")[0]
-    sentenceCardWord.innerHTML=res[cardNo].sentence
-
-    const ratingCard=document.getElementById("card-container-1").getElementsByClassName("rating-card")[0]
-    ratingCard.innerHTML=""
-    
-    // checkButton.classList.remove("hidden")
-
-    
-
-    for (let i = 0; i < res[cardNo].rating; i++) {
-      addStarToCard()
-    }
-    //ratingCard=null;
-
-    germanCardWord=document.getElementById("card-container-2").getElementsByClassName("german-card-word")[0]
-    germanCardWord.innerHTML=res[cardNo-1].german
-
-    turkishCardWord=document.getElementById("card-container-2").getElementsByClassName("turkish-card-word")[0]
-    turkishCardWord.innerHTML=res[cardNo-1].turkish
-
-    sentenceCardWord=document.getElementById("card-container-2").getElementsByClassName("sentence-card-word")[0]
-    sentenceCardWord.innerHTML=res[cardNo-1].sentence
-
-
-    germanCardWord=document.getElementById("card-container-3").getElementsByClassName("german-card-word")[0]
-    germanCardWord.innerHTML=res[cardNo+1].german
-
-    turkishCardWord=document.getElementById("card-container-3").getElementsByClassName("turkish-card-word")[0]
-    turkishCardWord.innerHTML=res[cardNo+1].turkish
-
-    sentenceCardWord=document.getElementById("card-container-3").getElementsByClassName("sentence-card-word")[0]
-    sentenceCardWord.innerHTML=res[cardNo+1].sentence
-    // console.log('wordsArr[0].german :>> ', wordsArr);
-    // console.log('word :>> ', word);
-  })
-  // getElementsByClassName("german-card-word")[0].innerHTML="a"
+  const sentenceCardWord = document.getElementById(cardDiv).getElementsByClassName("sentence-card-word")[0]
+  sentenceCardWord.innerHTML = wordsArr[no].sentence
+}
+const createCards = () => {
+  //created card1
+  addCard("card-container-1", cardNo);
+  const ratingCard = document.getElementById("card-container-1").getElementsByClassName("rating-card")[0]
+  ratingCard.innerHTML = ""
+  for (let i = 0; i < wordsArr[cardNo].rating; i++) {
+    addStarToCard()
+  }
+  //created card2
+  addCard("card-container-2", cardNo - 1);
+  //created card3
+  addCard("card-container-3", cardNo + 1);
 }
 
-  //listen the cards whether on clicked
-document.getElementById("card-container-3").addEventListener("click",()=>{
-  getWords
-  .then(res=>{
-    if(cardNo+1<res.length){
-      cardNo++;
-      // console.log('res.length :>> ', res.length);
-    }
-    if(cardNo+1==res.length) document.getElementById("card-container-3").classList.add("hidden");
-    document.getElementById("card-container-2").classList.remove("hidden");
-    addToCard();
-  })
-  console.log('cardNo 3:>> ', cardNo);
+//listen the cards whether on clicked
+document.getElementById("card-container-3").addEventListener("click", () => {
+  if (cardNo + 1 < wordsArr.length) cardNo++;
+  if (cardNo + 1 == wordsArr.length) document.getElementById("card-container-3").classList.add("hidden");
+  document.getElementById("card-container-2").classList.remove("hidden");
+  createCards();
 })
+console.log('cardNo 3:>> ', cardNo);
+// })
 
-document.getElementById("card-container-2").addEventListener("click",()=>{
-  if(cardNo>0) cardNo--;
-  if(cardNo==0) document.getElementById("card-container-2").classList.add("hidden");
+document.getElementById("card-container-2").addEventListener("click", () => {
+  if (cardNo > 0) cardNo--;
+  if (cardNo == 0) document.getElementById("card-container-2").classList.add("hidden");
   document.getElementById("card-container-3").classList.remove("hidden");
   console.log('cardNo 2:>> ', cardNo);
-  addToCard();
+  createCards();
 })
 
 //listen the check button
-const listenCheckButton=()=>{
+const listenCheckButton = () => {
   console.log('listening :>> ');
   let checkButton = document.getElementById("card-container-1").getElementsByClassName("check-button")[0]
-  console.log('checkButton :>> ',checkButton);
-  checkButton.addEventListener("click",()=>{
-  // getWords
-  //   .then(res=>{
-  const inputValue = document.getElementById("card-container-1").getElementsByClassName("input-value")[0].value
-  console.log('checkButton :>> ', checkButton); 
-  compare(inputValue);
-  console.log('called compare :>> ');
-  // })
+  console.log('checkButton :>> ', checkButton);
+  checkButton.addEventListener("click", () => {
+    const inputValue = document.getElementById("card-container-1").getElementsByClassName("input-value")[0].value
+    document.getElementById("card-container-1").getElementsByClassName("input-value")[0].value=""
+    compare(inputValue);
   })
 }
 
 // console.log('firebase :>> ', firebase);
-        // let header = document.getElementsByClassName("test");
-        // let dbRef = firebase.database().ref().child("text");
-        // dbRef.on("value", snap=>header.innerText=snap.val())
+// let header = document.getElementsByClassName("test");
+// let dbRef = firebase.database().ref().child("text");
+// dbRef.on("value", snap=>header.innerText=snap.val())
